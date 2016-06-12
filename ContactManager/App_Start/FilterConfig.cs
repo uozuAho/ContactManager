61da89c0d3d674d8626cd59e5919699bc82c5811
Warwick Stone <uozu.aho@gmail.com>
2016-06-12 18:55:56 +1000

    require auth for all actions by default (whitelist home/index), require https

--- ContactManager/App_Start/FilterConfig.cs
+++ ContactManager/App_Start/FilterConfig.cs
@@ -1,5 +1,4 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace ContactManager
 {
@@ -8,6 +7,8 @@ namespace ContactManager
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new AuthorizeAttribute());
+            filters.Add(new RequireHttpsAttribute());
         }
     }
 }
--- ContactManager/Controllers/HomeController.cs
+++ ContactManager/Controllers/HomeController.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace ContactManager.Controllers
 {
     public class HomeController : Controller
     {
+        [AllowAnonymous]
         public ActionResult Index()
         {
             return View();
