dc85a82792b12eebd3811cbc0d2ea8d695011d9a
Warwick <uozu.aho@gmail.com>
2016-08-29 05:37:32 -0700

    add AI telemetry to uncaught exceptions

--- ContactManager/App_Start/FilterConfig.cs
+++ ContactManager/App_Start/FilterConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web.Mvc;
+﻿using ContactManager.Controllers;
+using System.Web.Mvc;
 
 namespace ContactManager
 {
@@ -6,7 +7,7 @@ namespace ContactManager
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            filters.Add(new AiHandleErrorAttribute());
             filters.Add(new RequireHttpsAttribute());
         }
     }
